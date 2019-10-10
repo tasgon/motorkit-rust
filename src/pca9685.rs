6dc899e40eb85129e3215e2fca6e9ef5b4945ed9
telastrus <10052313+telastrus@users.noreply.github.com>
2019-10-10 01:23:02 -0400

    added constants

--- src/pca9685.rs
+++ src/pca9685.rs
@@ -1,6 +1,32 @@
 use rppal::i2c::I2c;
 
 
+// The following constants taken from https://github.com/adafruit/Adafruit_Python_PCA9685/blob/master/Adafruit_PCA9685/PCA9685.py
+// Registers
+const PCA9685_ADDRESS: u16    = 0x40;
+const MODE1: u16              = 0x00;
+const MODE2: u16              = 0x01;
+const SUBADR1: u16            = 0x02;
+const SUBADR2: u16            = 0x03;
+const SUBADR3: u16            = 0x04;
+const PRESCALE: u16           = 0xFE;
+const LED0_ON_L: u16          = 0x06;
+const LED0_ON_H: u16          = 0x07;
+const LED0_OFF_L: u16         = 0x08;
+const LED0_OFF_H: u16         = 0x09;
+const ALL_LED_ON_L: u16       = 0xFA;
+const ALL_LED_ON_H: u16       = 0xFB;
+const ALL_LED_OFF_L: u16      = 0xFC;
+const ALL_LED_OFF_H: u16      = 0xFD;
+
+// Bits
+const RESTART: u16            = 0x80;
+const SLEEP: u16              = 0x10;
+const ALLCALL: u16            = 0x01;
+const INVRT: u16              = 0x10;
+const OUTDRV: u16             = 0x04;
+const SWRESET: u16            = 0x06;
+
 #[derive(Clone, Copy)]
 struct PWMChannel {
     frequency: u16,
